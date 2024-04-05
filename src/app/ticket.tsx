5285b1d31d5bbd94a28c42224fa17fa567325d30
Wagner Terry <goncalveswagner15@gmail.com>
2024-04-04 21:52:29 -0300

    chore : avançando na credencial

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -1,13 +1,47 @@
+import { View, StatusBar, Text, ScrollView, TouchableOpacity } from "react-native";
 import { Credential } from "@/components/credential";
 import { Header } from "@/components/header";
-import { View, StatusBar } from "react-native";
+import { FontAwesome } from "@expo/vector-icons"
+import { colors } from "@/styles/colors";
+import { Button } from "@/components/button";
 
 export default function Ticket() {
     return (
         <View className="flex-1 bg-green-500">
             <StatusBar barStyle="light-content" />
             <Header title="Minha Credencial" />
-            <Credential />
+            <ScrollView
+                className="-mt-28 -z-10"
+                contentContainerClassName="px-8 pb-8"
+                showsVerticalScrollIndicator={false}
+            >
+                <Credential />
+
+                <FontAwesome
+                    name="angle-double-down"
+                    color={colors.gray[300]}
+                    size={24}
+                    className="self-center my-6"
+                />
+
+                <Text className="text-white font-bold text-2xl mt-4">Compartilhar credencial</Text>
+                <Text className="text-white font-regular text-2base mt-1 mb-6">
+                    Mostre ao mundo que você vai participar do Unite Summit!
+                </Text>
+
+                <Button
+                    title="Compartilhar"
+                />
+
+                <TouchableOpacity
+                    activeOpacity={0.7}
+                    className="mt-10"
+                >
+                    <Text className="text-base text-white font-bold text-center">
+                        Remover Ingresso
+                    </Text>
+                </TouchableOpacity>
+            </ScrollView>
         </View>
     )
 }
